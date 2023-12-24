decbf1c38210671c840fe017c13c683a9bb764db
Justfemi <ajibadeolufemi94@gmail.com>
2023-12-24 03:08:12 +0100

    ch:modified blue box padding and closed nav onclick of nav-contents

--- assignment-3/app.js
+++ assignment-3/app.js
@@ -1,5 +1,7 @@
 const nav = document.querySelector('nav');
 const hamburger = document.querySelector('.icons');
+const navLinks = document.querySelectorAll('nav a');
+const navButton = document.querySelector('.nav-btn');
 
 hamburger.addEventListener('click', () => {
   nav.classList.toggle('show');
@@ -10,8 +12,19 @@ hamburger.addEventListener('click', () => {
   }
 });
 
-document.addEventListener('scroll', function () {
+document.addEventListener('scroll', () => {
   nav.classList.remove('show');
-  // hamburger.innerHTML = '&#9776;';
   hamburger.innerHTML = '<i class="bx bx-menu"></i>';
 });
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+    hamburger.innerHTML = '<i class="bx bx-menu"></i>';
+  });
+});
+
+navButton.addEventListener('click', () => {
+  nav.classList.remove('show');
+  hamburger.innerHTML = '<i class="bx bx-menu"></i>';
+})
